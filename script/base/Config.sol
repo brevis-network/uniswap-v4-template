e9c304836e73716359f0d13ee3ece4b88d4b7c05
Junda Liu <junda@celer.network>
2024-10-28 13:16:37 -0700

    deploy hook

--- script/base/Config.sol
+++ script/base/Config.sol
@@ -8,9 +8,9 @@ import {Currency} from "v4-core/src/types/Currency.sol";
 /// @notice Shared configuration between scripts
 contract Config {
     /// @dev populated with default anvil addresses
-    IERC20 constant token0 = IERC20(address(0x0165878A594ca255338adfa4d48449f69242Eb8F));
-    IERC20 constant token1 = IERC20(address(0xa513E6E4b8f2a923D98304ec87F64353C4D5C853));
-    IHooks constant hookContract = IHooks(address(0x0));
+    IERC20 constant token0 = IERC20(address(0x0dB4ceE042705d47Ef6C0818E82776359c3A80Ca));
+    IERC20 constant token1 = IERC20(address(0x7A46219950d8a9bf2186549552DA35Bf6fb85b1F));
+    IHooks constant hookContract = IHooks(address(0xaA5f84D2F6A2423E50b3e598F934f6626e12CAc0));
 
     Currency constant currency0 = Currency.wrap(address(token0));
     Currency constant currency1 = Currency.wrap(address(token1));
